fdf9ed5a2a5d24cb7dc354acad54ec62eea1d3ad
Dmitrii Morozov <33morozov@gmail.com>
2023-01-15 18:15:31 +0300

    Fixed Debug mode

--- menu_app/database.py
+++ menu_app/database.py
@@ -3,7 +3,6 @@ from sqlmodel import Session, create_engine
 
 
 # database_url = os.getenv('POSTGRES_DB_URL')
-DEBUG = os.getenv('DEBUG')
 database_url = "postgresql+psycopg2://postgres@localhost/food_menu"
 engine = create_engine(database_url, echo=True)
 
