b82b541982fb8c8e22c2a06ff2d886503aa3b612
Dmitrii Morozov <33morozov@gmail.com>
2023-01-17 16:36:20 +0300

    Add test

--- tests/test_menu_app.py
+++ tests/test_menu_app.py
@@ -30,11 +30,50 @@ def client_fixture(session: Session):
 def test_create_menu(client: TestClient):
     response = client.post(
             "/api/v1/menus/",
-            json={"title": "Menu 1", "description": "My menu description 1"},
+            json={"title": "Menu 1", "description": "Menu description 1"},
     )
     app.dependency_overrides.clear()
     data = response.json()
 
     assert response.status_code == 201
     assert data["title"] == "Menu 1"
-    assert data["description"] == "My menu description 1"
+    assert data["description"] == "Menu description 1"
+
+
+def test_create_menu_incomplete(client: TestClient):
+    # No description
+    response = client.post("/api/v1/menus", json={"title": "Menu 1"})
+    assert response.status_code == 422
+
+
+def test_create_menu_invalid(client: TestClient):
+    # title has an invalid type
+    response = client.post(
+        "/api/v1/menus",
+        json={
+            "title": {"message": "Menu 1"},
+            "description": "Menu description 1",
+        },
+    )
+    assert response.status_code == 422
+
+
+def test_read_menus(session: Session, client: TestClient):
+    menu_1 = Menu(title="Menu 1", description="Menu description 1")
+    menu_2 = Menu(title="Menu 2", description="Menu description 1")
+    session.add(menu_1)
+    session.add(menu_2)
+    session.commit()
+
+    response = client.get("/api/v1/menus")
+    data = response.json()
+
+    assert response.status_code == 200
+
+    assert len(data) == 2
+    assert data[0]["title"] == menu_1.title
+    assert data[0]["description"] == menu_1.description
+    assert data[0]["id"] == str(menu_1.id)
+    assert data[1]["title"] == menu_2.title
+    assert data[1]["description"] == menu_2.description
+    assert data[1]["id"] == str(menu_2.id)
