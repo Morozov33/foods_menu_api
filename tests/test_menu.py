f210f48665e10f928e9e915cbb4a20647036e31b
Dmitrii Morozov <33morozov@gmail.com>
2023-01-17 16:56:23 +0300

    Update test for Menu and fixtures

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -1,30 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
-from sqlmodel import Session, SQLModel, create_engine
-from sqlmodel.pool import StaticPool
-from menu_app.main import app, get_session, Menu, Submenu, Dish
-
-
-@pytest.fixture(name="session")
-def session_fixture():
-    engine = create_engine(
-        "sqlite://",
-        connect_args={"check_same_thread": False},
-        poolclass=StaticPool,
-    )
-    SQLModel.metadata.create_all(engine)
-    with Session(engine) as session:
-        yield session
-
-
-@pytest.fixture(name="client")
-def client_fixture(session: Session):
-    def get_session_override():
-        return session
-    app.dependency_overrides[get_session] = get_session_override
-    client = TestClient(app)
-    yield client
-    app.dependency_overrides.clear()
+from sqlmodel import Session
+from menu_app.main import Menu
 
 
 def test_create_menu(client: TestClient):
@@ -32,7 +9,6 @@ def test_create_menu(client: TestClient):
             "/api/v1/menus/",
             json={"title": "Menu 1", "description": "Menu description 1"},
     )
-    app.dependency_overrides.clear()
     data = response.json()
 
     assert response.status_code == 201
